08f8015b9f8b34bc20e1e402608f20c69afc44df
Mark-Emmanuel311 <markemmanuel311@gmail.com>
2025-02-05 22:18:05 +0300

    style up popup

--- js/script.js
+++ js/script.js
@@ -2,16 +2,11 @@ document.getElementById('year').innerText = new Date().getFullYear();
 
 function reachUs(x){
     let text = document.createElement('p');
-    let links = [];
-    for(let i = 0; i < 3; i++){
-
-    }
     let insert = x == 1? "to purchase your items" : "for any inquiry or anything you might need";
-
     text.classList.add("text");
-    text.innerText = `Hello our esteemed customer, reach us directly ${insert}
-                    `;
-    let message = document.querySelector(".message").appendChild(text);
+    text.innerText = `Hello our esteemed customer, reach us directly ${insert}`;
+    let first = document.querySelector(".message > button");
+    first.parentElement.insertBefore(text,first.nextSibling);
 }
 
-reachUs(1);
+reachUs(0);
