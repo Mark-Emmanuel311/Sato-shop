ac0706b84b0f9637e79266759b8717bb02a03db5
Mark-Emmanuel311 <markemmanuel311@gmail.com>
2025-02-05 21:18:32 +0300

    adding popup

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 document.getElementById('year').innerText = new Date().getFullYear();
+
+function reachUs(x){
+    let text = document.createElement('p');
+    let links = [];
+    for(let i = 0; i < 3; i++){
+
+    }
+    let insert = x == 1? "to purchase your items" : "for any inquiry or anything you might need";
+
+    text.classList.add("text");
+    text.innerText = `Hello our esteemed customer, reach us directly ${insert}
+                    `;
+    let message = document.querySelector(".message").appendChild(text);
+}
+
+reachUs(1);
